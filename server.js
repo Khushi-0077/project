0bac91421f4c30f2643138224dac57e99379ac46
Khushpreet Kaur <138706118+Khushi-0077@users.noreply.github.com>
2023-08-13 15:24:41 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// POST route for processing form data
+app.post('/process', (req, res) => {
+  const num1 = parseFloat(req.body.num1);
+  const num2 = parseFloat(req.body.num2);
+  const operation = req.body.operation;
+
+  let result;
+
+  switch (operation) {
+    case 'add':
+      result = num1 + num2;
+      break;
+    case 'subtract':
+      result = num1 - num2;
+      break;
+    case 'multiply':
+      result = num1 * num2;
+      break;
+    case 'divide':
+      result = num1 / num2;
+      break;
+    default:
+      result = 'Invalid operation';
+  }
+
+  res.send(result.toString());
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
+
+
